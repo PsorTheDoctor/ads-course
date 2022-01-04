9a1e1400c79be55c7c9d6532e87376a734218400
PsorTheDoctor <psor2.0@gmail.com>
2022-01-05 00:55:49 +0100

    sorting

--- src/com/wolkowycki/exercises/Main.java
+++ src/com/wolkowycki/exercises/Main.java
@@ -1,5 +1,8 @@
 package com.wolkowycki.exercises;
 
+import java.util.Comparator;
+import java.util.Vector;
+
 public class Main {
     // Returns the sum of the first n natural numbers
     static int sum(int n) {
@@ -39,7 +42,65 @@ public class Main {
         return false;
     }
 
+    // A table holds n sorted numbers.
+    // The task is to determine if the table contains two numbers
+    // whose sum is equal to the value of parameter x.
+    static boolean contains(int[] array, int x) {
+
+        int n = array.length;
+        int leftPivot = 0;
+        int rightPivot = 0;
+
+        // The sum of left and right pivots must be lower than n - 1,
+        // because it ensures they aren't refer to the same number
+        while (leftPivot + rightPivot < n - 1) {
+            int left = array[leftPivot];
+            int right = array[n - rightPivot - 1];
+
+            if (left + right == x) {
+                return true;
+            } else if (left + right < x) {
+                leftPivot++;
+            } else {
+                rightPivot++;
+            }
+        }
+        return false;
+    }
+
+    private static Vector<Integer> sort(Vector<Integer> v) {
+        v.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+        return v;
+    }
+
+    static boolean areAnagrams(String s1, String s2) {
+
+        boolean areAnagrams = false;
+
+        if (s1.length() == s2.length()) {
+            Vector<Integer> v1 = new Vector<>();
+            Vector<Integer> v2 = new Vector<>();
+
+            for (int i = 0; i < s1.length(); i++) {
+                v1.add((int) s1.charAt(i));
+                v2.add((int) s2.charAt(i));
+            }
+            if (sort(v1).equals(sort(v2))) {
+                areAnagrams = true;
+            }
+        }
+        return areAnagrams;
+    }
+
     public static void main(String[] args) {
-        System.out.println(evenSquares(1));
+
+        int[] a = {0, 1, 2, 3, 4};
+        System.out.println(contains(a, 7));
+        System.out.println(areAnagrams("least", "steal"));
     }
 }
